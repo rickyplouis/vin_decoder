6cdcec1560e0fdf49e760f2983e4dc8c3401f5bb
Ricardo <ricardoplouis@gmail.com>
2018-10-15 11:26:15 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# How To Use
+
+1. Install dependencies
+
+```$ npm install ```
+2. Use the default vin csv for reference or create your own (name must be vin.csv)
+
+3. Run the index file
+
+```$ node index.js ```
+
+4. Go drink some coffee and chill :D
---
+++ index.js
@@ -0,0 +1,48 @@
+const fs = require('fs');
+const csv = require("fast-csv");
+const vindec = require('vindec');
+
+const ws = fs.createWriteStream("validated.csv");
+
+const decodedVins = []
+
+const createBlankVin = (vin) => {
+  return {
+    vin,
+    valid: false,
+    wmi: '',
+    checkDigit: '',
+    vis: '',
+    region: '',
+    make: '',
+    year: '',
+    sequence_id: ''
+  };
+}
+
+const cleanVin = (vinObj) => {
+  Object.keys(vinObj).forEach(function(key,index) {
+    if (typeof vinObj[key] === 'undefined') {
+      vinObj[key] = false;
+    }
+    // key: the name of the object key
+    // index: the ordinal position of the key within the object
+  });
+  return vinObj;
+}
+
+csv
+  .fromPath('./vin.csv')
+  .on("data", function(data){
+    const decodedVin = cleanVin(vindec.decode(data[0]));
+    if (decodedVin.valid) {
+      decodedVins.push(decodedVin);
+    } else {
+      decodedVins.push(createBlankVin(decodedVin.vin))
+    }
+  })
+  .on("end", function(){
+    console.log('decodedVins', decodedVins);
+    csv.writeToPath("validated.csv", decodedVins)
+   console.log("done");
+  });
